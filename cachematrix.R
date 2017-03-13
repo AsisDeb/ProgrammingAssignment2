7fea5878ac124bdae6a29dc2e3fbb0a6aa1ba99a
Debasis <Debasis.Behura@gmail.com>
2017-03-13 12:05:24 +0530

    Commit for cloned cachematrix.R after modification

--- cachematrix.R
+++ cachematrix.R
@@ -1,15 +1,28 @@
-## Put comments here that give an overall description of what your
-## functions do
-
-## Write a short comment describing this function
-
+## This function creates a list of functions to set and get the matrix and 
+## also set and get the value of its inverse
 makeCacheMatrix <- function(x = matrix()) {
-
+  inv <- NULL
+  set <- function(y = matrix ()) {
+    x <<- y
+    inv <<- NULL
+  }
+  get <- function() x
+  setinv <- function(solve) inv <<- solve
+  getinv <- function() inv
+  z = list(set = set, get = get, setinv = setinv, getinv = getinv)
+  cacheSolve(z)
 }
-
-
-## Write a short comment describing this function
-
-cacheSolve <- function(x, ...) {
-        ## Return a matrix that is the inverse of 'x'
+## This function returns the inverse of the matrix passed from the 
+## makeCacheMatrix function after checking whether the inverse exists
+## and whether the matrix has changed
+cacheSolve <- function(z) {
+  inv <- z$getinv()
+  if(!is.null(inv) && z$get() != z$set) {
+  message("getting cached data")
+  return(inv)
+  }
+  data <- z$get()
+  inv <- solve(data)
+  z$setinv(inv)
+  inv
 }
